42603fd1a7ddfd0fd9a03ac70fadea62581fe1ae
kaitoharuoka <73736001+kaitoharuoka@users.noreply.github.com>
2021-04-05 13:12:43 +0900

    テンプレート文字列を追記

--- src/index.js
+++ src/index.js
@@ -44,3 +44,18 @@
 // val5[0] = "bird";
 // val5.push("monkey");
 // console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+const name = "Kaito";
+const age = 21;
+//「私の名前はKaitoです。年齢は21歳です。」
+
+//従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+console.log(message1);
+
+//テンプレート文字列を用いた方法
+const message2 = `私の名前${name}です。年齢は${age}です。`;
+console.log(message2);
